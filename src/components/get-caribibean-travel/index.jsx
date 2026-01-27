bfc3994a5e46cc145d2bdfbc8412e42e2bb8ac03
asad-fleek <asad.mahmood@pendoah.ai>
2026-01-28 03:05:10 +0500

    projectdone

--- src/components/get-caribibean-travel/index.jsx
+++ src/components/get-caribibean-travel/index.jsx
@@ -5,7 +5,7 @@ function GetTravel() {
 
   return (
     <section className="flex justify-center items-center pt-8 mb-20">
-      <div className="relative flex flex-col items-center justify-center bg-[url('images/get-your/familyframe.png')] w-[1096px] h-[343px] bg-cover bg-center rounded-lg">
+      <div className="relative flex flex-col items-center justify-center bg-family w-[1096px] h-[343px] bg-cover bg-center rounded-lg">
         <h1 className="text-center font-primary font-extrabold text-[48px] text-stone-50 leading-tight">Get Caribbean Travel Tips <br />+ Exclusive Deals</h1>
 
         <button className="mt-6 px-8 py-3 font-primary font-bold text-stone-50 bg-orange-500 rounded-lg hover:bg-orange-600 transition">Get Notified</button>
